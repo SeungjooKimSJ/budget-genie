db44220d0e924a33cd32e83fb69d76f4817a8482
Seungjoo Kim <sjskybone@gmail.com>
2025-04-18 16:26:47 -0700

    modified initial styleing and set up.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
         'mint-green': '#5FC9A4',
         'gold': '#FFD466',
         'coral': '#FF6B6B',
+        'soft-lavender': '#F3F4F9',
+        'soft-ivory': '#FFFCF7',
+      },
+      animation: {
+        'sparkle': 'sparkle 1.5s ease-in-out infinite',
+        'float': 'float 0.6s ease-in-out',
+      },
+      boxShadow: {
+        'genie': '0 0 15px rgba(92,106,196,0.2)',
       },
     },
   },
